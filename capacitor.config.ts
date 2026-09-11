93296df3572bfd66da2d5b23670b17d4db3ba610
johnrheytamares <johnrheynedamotamares@gmail.com>
2026-09-11 16:10:18 +0800

    Initial Commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.photogallery.mobile',
   appName: 'Photo_Gallery',
   webDir: 'dist'
 };
